e71a4a696b25711d324d98dc9c8fa30696fb52e9
Jan <janewaldable@gmail.com>
2020-12-06 14:58:13 +0100

    implemented active year pipe

---
+++ study-planner-app/src/app/planner/pipes/active-year.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ActiveYearPipe } from './active-year.pipe';
+
+describe('ActiveYearPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ActiveYearPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ study-planner-app/src/app/planner/pipes/active-year.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Year} from '../../shared/models';
+
+@Pipe({
+  name: 'activeYear',
+  pure: true
+})
+export class ActiveYearPipe implements PipeTransform {
+
+  transform(year: Year, currentTerm): boolean {
+    return year.terms.some(term => term.order === currentTerm);
+  }
+}
