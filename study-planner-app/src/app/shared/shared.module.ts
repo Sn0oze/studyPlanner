d03cb35632f34f8a1fb24b0ce65e3a6d4dc3fb54
Jan <janewaldable@gmail.com>
2020-12-07 20:06:00 +0100

    moved shared components into sub folder

--- study-planner-app/src/app/shared/shared.module.ts
+++ study-planner-app/src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {StatusBarComponent} from './status-bar/status-bar.component';
-import {PageHeaderComponent} from './page-header/page-header.component';
-import {ChartsModule} from './charts/charts.module';
+import {StatusBarComponent} from './components/status-bar/status-bar.component';
+import {PageHeaderComponent} from './components/page-header/page-header.component';
+import {ChartsModule} from './components/charts/charts.module';
 import { CategoryColorPipe } from './pipes/category-color.pipe';
 
 @NgModule({
