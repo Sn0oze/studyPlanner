ad8e6c9f533016c957266de1ebc818fcdb855d4d
Jan <janewaldable@gmail.com>
2020-12-07 20:18:34 +0100

    added placement type

--- study-planner-app/src/app/shared/models/studyPlan.model.ts
+++ study-planner-app/src/app/shared/models/studyPlan.model.ts
@@ -1,14 +1,16 @@
 export type Category = 'thesis' | 'general competence' | 'technological Specialisation' |'elective';
 
+export type Placement = 'spring' | 'fall';
+
 export interface Course {
   id: string;
   name: string;
-  placement: string;
+  placement: Placement;
   category: Category;
   points: number;
 }
 
-export interface Term   {
+export interface Term {
   order: number;
   placement: string;
   courses: Course[];
