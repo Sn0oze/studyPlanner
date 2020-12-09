d00a1158ea1ef3bc1e3787d2f98bf5c32e8e1f8d
Jan <janewaldable@gmail.com>
2020-12-09 22:59:10 +0100

    changed point-total.pipe.ts to accept term or academic year object

--- study-planner-app/src/app/planner/pipes/point-total.pipe.ts
+++ study-planner-app/src/app/planner/pipes/point-total.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {Course, AcademicYear} from '../../shared/models';
+import {Course, AcademicYear, Term, instanceOfTerm, instanceOfAcademicYear} from '../../shared/models';
 
 @Pipe({
   name: 'pointTotal',
@@ -9,14 +9,16 @@ export class PointTotalPipe implements PipeTransform {
 
   private pointsAccumulator = (sum: number, course: Course) => sum + course.points;
 
-  public transform(year: AcademicYear, order?: number): number {
-    if (order) {
-      const courses = year.terms.find(term => term.order === order)?.courses || [];
-      return courses.reduce(this.pointsAccumulator, 0);
-    } else {
-      year.terms
+  public transform(period: AcademicYear | Term): number {
+    let result = 0;
+    if (instanceOfTerm(period)) {
+      result = period.courses.reduce(this.pointsAccumulator, 0);
+    }
+    else if (instanceOfAcademicYear(period)) {
+      result = period.terms
         .map(term => term.courses.reduce(this.pointsAccumulator, 0))
         .reduce((sum, termTotal) => sum + termTotal, 0);
     }
+    return result;
   }
 }
