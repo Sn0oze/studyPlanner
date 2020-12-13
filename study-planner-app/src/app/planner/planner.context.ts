c816cc9aefce25e68dd5c835519a163083719dd9
Jan <janewaldable@gmail.com>
2020-12-13 21:33:30 +0100

    added behaviour subject to planner context to communicate which module in the course calendar is selected

--- study-planner-app/src/app/planner/planner.context.ts
+++ study-planner-app/src/app/planner/planner.context.ts
@@ -4,7 +4,8 @@ import {Course} from '../shared/models';
 
 @Injectable()
 export class PlannerContext {
-  public  dragSelection = new BehaviorSubject<Course>(null);
+  private  dragSelection = new BehaviorSubject<Course | null>(null);
+  private  moduleSelection = new BehaviorSubject<string | null>(null);
   constructor() {}
 
   public getDragSelection(): Observable<Course> {
@@ -13,5 +14,13 @@ export class PlannerContext {
 
   public updateDragSelection(course: Course): void {
     this.dragSelection.next(course);
+  }
+
+  public getModuleSelection(): Observable<string> {
+    return this.moduleSelection.asObservable();
+  }
+
+  public updateModuleSelection(module: string): void {
+    this.moduleSelection.next(module);
   }
 }
