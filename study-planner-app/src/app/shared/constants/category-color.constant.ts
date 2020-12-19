6a3488590089b2b0d2cce2fd72f33006e8bebff2
Jan <janewaldable@gmail.com>
2020-12-19 18:32:58 +0100

    fixed spelling

--- study-planner-app/src/app/shared/constants/category-color.constant.ts
+++ study-planner-app/src/app/shared/constants/category-color.constant.ts
@@ -1,13 +1,13 @@
-export enum CategoryColors {
+export enum CategoryColor {
   THESIS = '#f3993e',
-  GENERAL_COMPETENCES = '#e83f48',
+  GENERAL_COMPETENCE = '#e83f48',
   TECHNOLOGICAL_SPECIALISATION = '#792d8e',
   ELECTIVE = '#3042ea'
 }
 
 export const CategoryColorMap = {
-  thesis: CategoryColors.THESIS,
-  general_competence: CategoryColors.GENERAL_COMPETENCES,
-  technological_specialisation : CategoryColors.TECHNOLOGICAL_SPECIALISATION,
-  elective: CategoryColors.ELECTIVE
+  thesis: CategoryColor.THESIS,
+  general_competence: CategoryColor.GENERAL_COMPETENCE,
+  technological_specialisation : CategoryColor.TECHNOLOGICAL_SPECIALISATION,
+  elective: CategoryColor.ELECTIVE
 };
